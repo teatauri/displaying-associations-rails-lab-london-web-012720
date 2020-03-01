95b66bcc2979a33c8533f7da9288a66e64435be5
github username <github email address>
2020-03-01 20:36:46 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,8 +1,11 @@
 class SongsController < ApplicationController
+
   def index
+    @songs = Song.all
   end
 
   def show
+    @song = Song.find(params[:id])
   end
 
   def new
@@ -11,7 +14,7 @@ class SongsController < ApplicationController
 
   def create
     @song = Song.new(song_params)
-    @song.artist = Artist.find_or_create_by(artist_params)
+    @song.artist = Song.find_or_create_by(artist_params)
 
     if @song.save
       redirect_to @song
